093ff3c1aa281bea08c20f50e6a77b4b87387a51
Rich Higgins <hello@iamrichhiggins.com>
2017-12-04 17:25:55 +0000

    added build config

--- fractal.js
+++ fractal.js
@@ -29,3 +29,8 @@ fractal.docs.set('path', path.join(__dirname, 'docs'));
  * Tell the Fractal web preview plugin where to look for static assets.
  */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+/*
+ * Set the static HTML build destination
+ */
+fractal.web.set('builder.dest', __dirname + '/build');
